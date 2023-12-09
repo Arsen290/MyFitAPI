9a30b78739ce93a5b77c95744f50388866f2b5f1
Arsen290 <arsen290@ukr.net>
2023-12-09 21:23:29 +0100

    Initialize a project

---
+++ src/main/java/cz/vladarsen/MyFitAPI/MyFitApiApplication.java
@@ -0,0 +1,13 @@
+package cz.vladarsen.MyFitAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFitApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFitApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cz/vladarsen/MyFitAPI/MyFitApiApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.vladarsen.MyFitAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFitApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
