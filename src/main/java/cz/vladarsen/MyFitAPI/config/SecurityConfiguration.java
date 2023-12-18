22ea2cf6a409b6a0e79ea58c1e05d07147be631a
Arsen290 <arsen290@ukr.net>
2023-12-18 21:30:29 +0100

    create Security configuration

--- src/main/java/cz/vladarsen/MyFitAPI/config/SecurityConfiguration.java
+++ src/main/java/cz/vladarsen/MyFitAPI/config/SecurityConfiguration.java
@@ -1,5 +1,38 @@
 package cz.vladarsen.MyFitAPI.config;
 
-public class SecurityConfiguration{
+import jakarta.servlet.Filter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration{
+    private final Filter jwtAuthenticationFilter;
+    private final AuthenticationManager authenticationProvider;
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .requestMatchers("").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authenticationManager(authenticationProvider)
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
 }
