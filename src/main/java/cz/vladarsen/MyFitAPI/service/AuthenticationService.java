32624a6e8a4342f1be4669c570fda9c464a94982
Arsen290 <arsen290@ukr.net>
2023-12-18 22:41:10 +0100

    create Auth service and implement register method before

--- src/main/java/cz/vladarsen/MyFitAPI/service/AuthenticationService.java
+++ src/main/java/cz/vladarsen/MyFitAPI/service/AuthenticationService.java
@@ -55,6 +55,7 @@ public class AuthenticationService {
         return AuthenticationResponse.builder()
                 .token(JwtToken)
                 .build();
+
     }
 
     public AuthenticationResponse authenticate(AuthenticationResponse request) {
