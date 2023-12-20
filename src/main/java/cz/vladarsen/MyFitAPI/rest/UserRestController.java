21ee9658ba67df5778dbce162606b29b6d71550d
Arsen290 <arsen290@ukr.net>
2023-12-20 16:25:52 +0100

    create UserRestController

---
+++ src/main/java/cz/vladarsen/MyFitAPI/rest/UserRestController.java
@@ -0,0 +1,41 @@
+package cz.vladarsen.MyFitAPI.rest;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/user")
+@RequiredArgsConstructor
+public class UserRestController {
+    private final UserService userService;
+
+    // Endpoint to get the details of the currently authenticated user
+    @GetMapping("/profile")
+    public ResponseEntity<UserDto> getUserProfile() {
+        UserDto userDto = userService.getCurrentUserProfile();
+        return ResponseEntity.ok(userDto);
+    }
+
+    // Endpoint to update user details
+    @PutMapping("/update")
+    public ResponseEntity<UserDto> updateUserProfile(@RequestBody UpdateUserRequest updateUserRequest) {
+        UserDto updatedUser = userService.updateUserProfile(updateUserRequest);
+        return ResponseEntity.ok(updatedUser);
+    }
+
+    // Endpoint to change the user's password
+    @PostMapping("/change-password")
+    public ResponseEntity<Void> changePassword(@RequestBody ChangePasswordRequest changePasswordRequest) {
+        userService.changeUserPassword(changePasswordRequest);
+        return ResponseEntity.ok().build();
+    }
+
+    // Endpoint to delete the user's account
+    @DeleteMapping("/delete")
+    public ResponseEntity<Void> deleteUserAccount() {
+        userService.deleteUserAccount();
+        return ResponseEntity.ok().build();
+    }
+
+}
