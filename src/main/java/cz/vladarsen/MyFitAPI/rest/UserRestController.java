b92bf3bcbb5d5000f491b1a589def4812c2a9949
Arsen290 <arsen290@ukr.net>
2023-12-23 15:10:18 +0100

    create possibilities to update users profile and password, installation time update in db

--- src/main/java/cz/vladarsen/MyFitAPI/rest/UserRestController.java
+++ src/main/java/cz/vladarsen/MyFitAPI/rest/UserRestController.java
@@ -2,6 +2,8 @@ package cz.vladarsen.MyFitAPI.rest;
 
 import cz.vladarsen.MyFitAPI.DTO.UserDTO;
 import cz.vladarsen.MyFitAPI.config.JwtService;
+import cz.vladarsen.MyFitAPI.request.ChangePasswordRequest;
+import cz.vladarsen.MyFitAPI.request.UpdateUserRequest;
 import cz.vladarsen.MyFitAPI.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -28,21 +30,29 @@ public class UserRestController {
         return ResponseEntity.ok(userDto);
     }
 
-    // Endpoint to update user details
-//    @PutMapping("/update")
-//    public ResponseEntity<UserDto> updateUserProfile(@RequestBody UpdateUserRequest updateUserRequest) {
-//        UserDto updatedUser = userService.updateUserProfile(updateUserRequest);
-//        return ResponseEntity.ok(updatedUser);
-//    }
-//
-//    // Endpoint to change the user's password
-//    @PostMapping("/change-password")
-//    public ResponseEntity<Void> changePassword(@RequestBody ChangePasswordRequest changePasswordRequest) {
-//        userService.changeUserPassword(changePasswordRequest);
-//        return ResponseEntity.ok().build();
-//    }
-//
-    // Endpoint to delete the user's account
+    //     Endpoint to update user details
+    @PutMapping("/update")
+    public ResponseEntity<UserDTO> updateUserProfile(
+            @RequestBody UpdateUserRequest updateUserRequest,
+            Authentication authentication
+    ) {
+        String username = authentication.getName();
+        UserDTO updatedUser = userService.updateUserProfile(username, updateUserRequest);
+        return ResponseEntity.ok(updatedUser);
+    }
+
+    // Endpoint to change the user's password
+    @PostMapping("/change-password")
+    public ResponseEntity<Void> changePassword(
+            @RequestBody ChangePasswordRequest changePasswordRequest,
+            Authentication authentication) {
+        String username = authentication.getName();
+
+        userService.changeUserPassword(username,changePasswordRequest);
+        return ResponseEntity.ok().build();
+    }
+
+    //  Endpoint to delete the user's account
     @DeleteMapping("/delete")
     public ResponseEntity<Void> deleteUserAccount(Authentication authentication) {
         String username = authentication.getName();
--- src/main/java/cz/vladarsen/MyFitAPI/service/UserService.java
+++ src/main/java/cz/vladarsen/MyFitAPI/service/UserService.java
@@ -3,16 +3,26 @@ package cz.vladarsen.MyFitAPI.service;
 import cz.vladarsen.MyFitAPI.DTO.UserDTO;
 import cz.vladarsen.MyFitAPI.entity.User;
 import cz.vladarsen.MyFitAPI.repository.UserRepository;
+import cz.vladarsen.MyFitAPI.request.ChangePasswordRequest;
+import cz.vladarsen.MyFitAPI.request.UpdateUserRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class UserService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public UserDTO getCurrentUserProfile(String username){
         User user = userRepository.findByUsername(username)
@@ -28,4 +38,50 @@ public class UserService {
         log.info("IN delete - user with username: {} successfully deleted",username);
     }
 
+    public UserDTO updateUserProfile(String username, UpdateUserRequest updateUserRequest) {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+        // Update only if the field in the request is not null
+        if (updateUserRequest.getFirstName() != null) {
+            user.setFirstName(updateUserRequest.getFirstName());
+        }
+        if (updateUserRequest.getLastName() != null) {
+            user.setLastName(updateUserRequest.getLastName());
+        }
+        if (updateUserRequest.getEmail() != null) {
+            user.setEmail(updateUserRequest.getEmail());
+        }
+
+        LocalDateTime localDateTime = LocalDateTime.now();
+        Date currentDate = Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant());
+        user.setUpdated(currentDate);
+
+        userRepository.save(user);
+        log.info("IN update - user with username: {} successfully updated at {}",username, currentDate);
+        return UserDTO.from(user);
+    }
+
+    public void changeUserPassword(String username, ChangePasswordRequest changePasswordRequest) {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+
+
+        // Check if the old password matches the current password
+        if (!passwordEncoder.matches(changePasswordRequest.getOldPassword(), user.getPassword())) {
+            log.warn("Old password provided does not match the current password.");
+            throw new BadCredentialsException("Old password is incorrect");        }
+
+
+        // Update the password
+        user.setPassword(passwordEncoder.encode(changePasswordRequest.getNewPassword()));
+
+        LocalDateTime localDateTime = LocalDateTime.now();
+        Date currentDate = Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant());
+        user.setUpdated(currentDate);
+
+        userRepository.save(user);
+
+
+        log.info("IN changeUserPassword - user with username: {} successfully changed password at {}" ,username,currentDate);
+    }
 }
