b88604c2c744339ba01c326c64308efabf035569
Arsen290 <arsen290@ukr.net>
2023-12-29 13:03:59 +0100

    try to resolve problem with docker

--- src/main/java/cz/vladarsen/MyFitAPI/config/ApplicationConfig.java
+++ src/main/java/cz/vladarsen/MyFitAPI/config/ApplicationConfig.java
@@ -2,9 +2,20 @@ package cz.vladarsen.MyFitAPI.config;
 
 
 import cz.vladarsen.MyFitAPI.repository.UserRepository;
+import jakarta.persistence.EntityManagerFactory;
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.orm.jpa.JpaProperties;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -13,8 +24,15 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
 
 @Configuration
+@EnableJpaRepositories(
+        basePackages = "cz.vladarsen.MyFitAPI.repository"
+)
+@EntityScan("cz.vladarsen.MyFitAPI.entity")
 @RequiredArgsConstructor
 public class ApplicationConfig {
     private final UserRepository repository;
@@ -42,4 +60,27 @@ public class ApplicationConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+//    @Primary
+//    @Bean(name = "entityManagerFactory")
+//    public LocalContainerEntityManagerFactoryBean entityManagerFactory(
+//            EntityManagerFactoryBuilder builder,
+//            DataSource dataSource,
+//            JpaProperties jpaProperties
+//    ) {
+//        return builder
+//                .dataSource(dataSource)
+//                .packages("cz.vladarsen.MyFitAPI.entity") // Пакет, где находятся ваши JPA-сущности
+//                .properties(jpaProperties.getProperties())
+//                .persistenceUnit("myFitPersistenceUnit")
+//                .build();
+//    }
+//
+//    @Primary
+//    @Bean(name = "transactionManager")
+//    public PlatformTransactionManager transactionManager(
+//            @Qualifier("entityManagerFactory") EntityManagerFactory entityManagerFactory
+//    ) {
+//        return new JpaTransactionManager(entityManagerFactory);
+//    }
 }
--- src/main/java/cz/vladarsen/MyFitAPI/repository/UserRepository.java
+++ src/main/java/cz/vladarsen/MyFitAPI/repository/UserRepository.java
@@ -2,9 +2,10 @@ package cz.vladarsen.MyFitAPI.repository;
 
 import cz.vladarsen.MyFitAPI.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+@Repository
 public interface UserRepository extends JpaRepository<User,Long> {
    Optional<User> findByUsername(String username);
 
